7fd89d330660fcd766bf2188fde4e634edee680e
anu-xo <anuradhaa.vits@gmail.com>
2026-07-16 20:21:26 +0530

    day 11

--- packages/web/src/pages/Register.jsx
+++ packages/web/src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
-import api from '../api/axios';
+import api from "../services/api.js";
 import { useAuthStore } from '../store/authStore.js';
 
 const registerSchema = z.object({
